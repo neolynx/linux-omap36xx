71c30024e2a4d9661e16d493c78e802fbb2ff540
Nishanth Menon <nm@ti.com>
2012-06-17 16:15:11 -0500

    PM / AVS: SmartReflex: introduce notifiers flags
    SmartReflex IP V1 and V2 have different registers and offsets.
    Currently, we pass the status as is to the class driver. However,
    since we don't pass the version of the underlying SR hardware
    to the Class driver, it will not be unable to make consistent
    sense of the status bits coming over to it.
    
    A class driver should be able to function without dependency
    on the exact IP version it is actually running on. We hence
    introduce our own translation in software level for a generic
    notification flag.
    
    As part of this change, we will now call the notifier iff we get
    a match with the notifier flags that the class driver requested.
    
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Andrii Tseglytskyi <andrii.tseglytskyi@ti.com>

--- include/linux/power/smartreflex.h
+++ include/linux/power/smartreflex.h
@@ -160,6 +160,12 @@ enum sr_instance {
 	OMAP_SR_NR,
 };
 
+/* Smart reflex notifiers */
+#define SR_NOTIFY_MCUDISACK		BIT(3)
+#define SR_NOTIFY_MCUBOUND		BIT(2)
+#define SR_NOTIFY_MCUVALID		BIT(1)
+#define SR_NOTIFY_MCUACCUM		BIT(0)
+
 enum omap_sr_calibration_period {
 	AVS_CALIBRATION_BOOT = 1,
 	AVS_CALIBRATION_PERIODIC,
