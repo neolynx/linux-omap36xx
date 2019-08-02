032739d961418206c03812854b291a9106aa55e0
André Roth <neolynx@gmail.com>
2019-08-02 05:54:00 +0200

    replace devm_request_and_ioremap
    Signed-off-by: André Roth <neolynx@gmail.com>

--- drivers/power/avs/omap_vp.c
+++ drivers/power/avs/omap_vp.c
@@ -893,7 +893,7 @@ static int omap_vp_probe(struct platform_device *pdev)
 
 	pname = "base-address";
 	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, pname);
-	base = devm_request_and_ioremap(dev, res);
+	base = devm_ioremap_resource(dev, res);
 	if (!base) {
 		dev_err(dev, "Unable to map '%s'\n", pname);
 		return -EADDRNOTAVAIL;
